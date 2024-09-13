a248c74e9f3575560cde10b28666a33921ef4cb8
Yayat Ruhiyat <renaxco@gmail.com>
2024-09-13 08:46:54 +0700

    add persegi code

---
+++ persegi.py
@@ -0,0 +1,11 @@
+print('='*40)
+print('RUMUS PERSEGI')
+print('='*40)
+
+sisi = int(input('Masukkan sisi : '))
+
+luas = sisi * sisi
+keliling  = 4 * sisi
+
+print('Luas persegi\t\t: ', luas)
+print('Keliling persegin\t: ', keliling)
